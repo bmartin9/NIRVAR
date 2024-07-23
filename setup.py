179d9a0076bb925176cc2338540059add70cb776
Brendan Martin <b.t.martin@protonmail.com>
2024-07-23 09:49:46 +0100

    update setup.py to 1.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 setup(
     name='nirvar',
     packages=find_packages(),
-    version='1.0.0',
+    version='1.1.0',
     description='Network Informed Restricted Vector Autoregression',
     author='Brendan Martin',
     license='MIT',
